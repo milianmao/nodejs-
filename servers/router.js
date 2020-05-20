f7e3c0054ba6eb10f547e5d0a063f63966a23031
milianmao <1145909208@qq.com>
2020-05-20 16:26:10 +0800

    login

--- servers/router.js
+++ servers/router.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var querystring = require('querystring');
 var mysql = require('mysql');
+var bodyparser = require('body-parser');
 var router = express.Router();
 var con = mysql.createConnection({
     host: 'localhost',
@@ -8,12 +10,32 @@ var con = mysql.createConnection({
     database: 'studentmanger'
 });
 con.connect();
-
-
-
-
-
-
+//
+router.post('/login',function(req,res){
+    // console.log(req.body);
+    var data = JSON.stringify(req.body);
+    data = JSON.parse(data);
+    var username = data.username;
+    var password = data.password; 
+    var sql = `select password from manger where username = "${username}"`;
+    con.query(sql,function(err,result){
+        if(err) {
+            console.log("没有此用户");
+            return res.sendStatus(600);
+        }
+        var dataString = JSON.stringify(result);
+        var data = JSON.parse(dataString);
+        var temp = data[0].password;
+        if(temp!=password){
+            console.log("密码错误");
+            return res.sendStatus(400);
+        }else{
+            console.log("登录成功");
+            res.sendStatus(200);
+        }
+    });
+})
+//查询所有学生
 router.get('/',function(req,res){
     var sql = 'select * from student';
     con.query(sql,function(err,result){
